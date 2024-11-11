d352421d6223701740bb117597e247439375823b
Letícia Magalhães <leticiamassoleni@gmail.com>
2024-11-11 10:19:48 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./Components/Header/Header"
+
+export default function App(){
+  return(
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route/>
+      <Route/>
+      <Route/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
